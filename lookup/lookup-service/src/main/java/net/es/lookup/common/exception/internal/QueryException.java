b876439eec5236dcdee3a463ce104d6e06f3f014
sowmya.connect <sowmya.connect@e2671f6c-99c7-5955-88b5-72929a92eb07>
2013-03-08 01:09:58 +0000

    QueryNormalizer, Exception classes

---
+++ lookup/lookup-service/src/main/java/net/es/lookup/common/exception/internal/QueryException.java
@@ -0,0 +1,12 @@
+package net.es.lookup.common.exception.internal;
+
+/**
+ * Author: sowmya
+ * Date: 3/6/13
+ * Time: 1:36 PM
+ */
+public class QueryException extends Exception {
+    public QueryException(String message){
+        super(message);
+    }
+}
---
+++ lookup/lookup-service/src/main/java/net/es/lookup/common/exception/internal/QueueException.java
@@ -0,0 +1,13 @@
+package net.es.lookup.common.exception.internal;
+
+/**
+ * Author: sowmya
+ * Date: 2/25/13
+ * Time: 3:33 PM
+ */
+public class QueueException extends Exception {
+    public QueueException(String message){
+        super(message);
+    }
+
+}
