bc2e360b1c007f73c720a4d82dc96c5ce633922c
sowmya.connect <sowmya.connect@e2671f6c-99c7-5955-88b5-72929a92eb07>
2013-03-27 00:14:46 +0000

    Adding ActiveMQ config

--- lookup/lookup-service/src/main/java/net/es/lookup/pubsub/amq/AMQueue.java
+++ lookup/lookup-service/src/main/java/net/es/lookup/pubsub/amq/AMQueue.java
@@ -5,6 +5,7 @@ import net.es.lookup.common.exception.internal.DataFormatException;
 import net.es.lookup.common.exception.internal.QueueException;
 import net.es.lookup.protocol.json.JSONMessage;
 import net.es.lookup.pubsub.Queue;
+import net.es.lookup.utils.QueueServiceConfigReader;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -18,24 +19,35 @@ import java.util.UUID;
  * Time: 3:23 PM
  */
 public class AMQueue extends Queue {
+
     private Connection connection;
     private Session session;
     private Topic topic;
     private MessageProducer producer;
     private String qid = "";
 
+
     /**
      * The constructor
      * 1) creates a queue - starts a connection, creates the producer
      * 2) Generates queueId which used as topic. Subscriber uses this qid to subscribe to queues
-     * */
+     */
     public AMQueue() throws QueueException {
 
         //TODO: Make ActiveMQ options configurable
         String user = ActiveMQConnection.DEFAULT_USER;
         String password = ActiveMQConnection.DEFAULT_PASSWORD;
-        String url = "tcp://localhost:61616";
-        ConnectionFactory factory = new ActiveMQConnectionFactory(user, password, url);
+        QueueServiceConfigReader configReader = QueueServiceConfigReader.getInstance();
+
+        String host = configReader.getHost();
+        int port = configReader.getPort();
+        String protocol = configReader.getProtocol();
+        long ttl = configReader.getTtl();
+        boolean isPersistent = configReader.isQueuePersistent();
+
+        String url = protocol + "://" + host + ":" + port;
+
+        ConnectionFactory factory = new ActiveMQConnectionFactory(user,password,url);
         try {
 
             connection = factory.createConnection();
@@ -50,8 +62,14 @@ public class AMQueue extends Queue {
             qid = UUID.randomUUID().toString();
             topic = session.createTopic(qid);
             producer = session.createProducer(topic);
-            producer.setTimeToLive(120000);
-            producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            producer.setTimeToLive(ttl);
+
+            if (isPersistent) {
+                producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+            } else {
+                producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            }
+
 
         } catch (JMSException e) {
             throw new QueueException(e.getMessage());
@@ -76,8 +94,6 @@ public class AMQueue extends Queue {
         } catch (JMSException e) {
             throw new QueueException(e.getMessage());
         }
-
-
     }
 
     public void close() throws QueueException {
