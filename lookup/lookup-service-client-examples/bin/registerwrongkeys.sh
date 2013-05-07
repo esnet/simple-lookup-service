caa4b8d92eb5d6698b4dc8b4aaea0888444dc4a7
sowmya.connect <sowmya.connect@e2671f6c-99c7-5955-88b5-72929a92eb07>
2013-05-07 00:44:14 +0000

    clean up

--- lookup/lookup-service-client-examples/bin/registerwrongkeys.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -v -H "Content-Type: application/json" -X POST -d '{"access-point":["http://localhost/accesspoint1"],"client-uuid":["myuuid"],"service-type":["bwctl"],"domain":["LHC","es.net"]}' http://localhost:8080/lookup/services
