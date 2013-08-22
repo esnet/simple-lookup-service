e4d3a5120d526f3dbab07ad7d8b5ab016a06b355
sowmyabalasubramanian@lbl.gov <sowmyabalasubramanian@lbl.gov@e2671f6c-99c7-5955-88b5-72929a92eb07>
2013-08-22 00:23:05 +0000

    moving installation and run commands into two different scripts

---
+++ lookup/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+APP_NAME="Simple Lookup Service"
+VERSION="0.1"
+SHORTNAME=lookup-service
+JAR_FILE=${SHORTNAME}-${VERSION}.one-jar.jar
+
+mvn --version > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Simple Lookup Service(sLS) uses Maven for packaging. Cannot detect maven in classpath. Please install Maven and set the PATH variable correctly."
+    exit 1
+fi
+
+BASEDIR="/opt/SimpleLookupService"
+SOURCEDIR="lookup-service"
+
+if [ -d "$BASEDIR" ]; then
+    rm -rf $BASEDIR
+fi
+
+mkdir $BASEDIR
+mkdir $BASEDIR/log
+mkdir $BASEDIR/out
+mkdir $BASEDIR/process
+mkdir $BASEDIR/target
+mkdir $BASEDIR/etc
+mkdir $BASEDIR/bin
+cp -r $SOURCEDIR/etc $BASEDIR/etc
+cp -r $SOURCEDIR/bin $BASEDIR/bin
+cp $SOURCEDIR/target/$JAR_FILE $BASEDIR/target/
+
+if [ $? -eq 0 ]; then
+    echo "Installed Simple Lookup Service(sLS) successfully!!\nNOTE: sLS requires MongoDB. Please ensure MongoDB is installed and running before starting sLS."
+else
+    echo "\n\nInstallation failed! Please correct errors and run the install script again as root."
+fi
+
+exit 0
