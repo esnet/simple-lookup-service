aee55c195b9677ed389b4d95df9a2d103226b595
sowmyabalasubramanian@lbl.gov <sowmyabalasubramanian@lbl.gov@e2671f6c-99c7-5955-88b5-72929a92eb07>
2013-10-09 22:37:22 +0000

    bug fix

--- lookup/simple-lookup-service-client-examples/bin/register.sh
+++ lookup/simple-lookup-service-client-examples/bin/register.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-curl -v -H "Content-Type: application/json" -X POST -d '{"type":["host"],"group-domains":["es.net"],"record-vp-locator":["abc5-bwctl.es.net"],"record-vp-name":["ESnet BWCTL"],"record-vp-type":[ "bwctl85"]}' http://sowmya-dev-vm.es.net:8090/lookup/records
+!/bin/sh
+curl -v -H "Content-Type: application/json" -X POST -d '{"type":["host"],"group-domains":["es.net"],"record-vp-locator":["abc9-bwctl.es.net"],"record-vp-name":["ESnet BWCTL"],"record-vp-type":[ "bwctl315"]}' http://antg-test-vm.es.net:8090/lookup/records
