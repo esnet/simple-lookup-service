9b7fc193f72df79574036aece483cf1847459878
sowmyabalasubramanian@lbl.gov <sowmyabalasubramanian@lbl.gov@e2671f6c-99c7-5955-88b5-72929a92eb07>
2014-05-23 00:41:09 +0000

    Including latest activemq release

--- lookup/simple-lookup-service-server/bin/run.sh
+++ lookup/simple-lookup-service-server/bin/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx256M -agentpath:/home/sowmya/jprofiler/bin/linux-x64/libjprofilerti.so=port=8849,nowait  -jar target/simple-lookup-service-server-1.1.one-jar.jar $*
+java -Xmx512M  -jar target/simple-lookup-service-server-1.1.one-jar.jar $*
