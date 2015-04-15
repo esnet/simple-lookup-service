17eea2f1b5066e448abe140b395f3c2d60cc631e
sowmya <sowmya@es.net>
2015-04-14 17:03:25 -0700

    Importing the correct package

--- lookup/simple-lookup-service-client-python/examples/queryExample.py
+++ lookup/simple-lookup-service-client-python/examples/queryExample.py
@@ -1,6 +1,6 @@
 from sls_client.records import *
 from sls_client.query import *
-import sls_client.find_ps_ma
+from sls_client.find_ps_ma import *
 
 #query all  services
 queryString = 'type=service'
@@ -9,7 +9,7 @@ print response
 
 #query for MA containing host data
 hostname = '202.179.252.18'
-result = find_ps_ma.get_ma_for_host(hostname)
+result = get_ma_for_host(hostname)
 print result
 
 
