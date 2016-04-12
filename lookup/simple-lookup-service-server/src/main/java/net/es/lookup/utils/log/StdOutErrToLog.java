8514f046ac9087a971553079f8ef38e7dc510ff7
sowmya <sowmya@es.net>
2016-04-12 12:57:46 -0700

    Adding a class that was left behind in previous commit

---
+++ lookup/simple-lookup-service-server/src/main/java/net/es/lookup/utils/log/StdOutErrToLog.java
@@ -0,0 +1,49 @@
+package net.es.lookup.utils.log;
+
+        import org.apache.log4j.Logger;
+
+        import java.io.PrintStream;
+
+/**
+ * Author: sowmya
+ * Date: 4/12/16
+ * Time: 12:15 PM
+ */
+public class StdOutErrToLog {
+
+
+        private static Logger LOG = Logger.getLogger(StdOutErrToLog.class);
+
+        public static void redirectStdOutErrToLog(){
+            System.setOut(createLoggingPrintStream(System.out));
+            System.setErr(createLoggingPrintStream(System.err));
+
+            System.out.println("Hi");
+        }
+
+        private static PrintStream createLoggingPrintStream(final PrintStream printStream) {
+
+            return new PrintStream(printStream){
+
+                public void print(String s){
+                    LOG.debug(s);
+                }
+
+                public void print(Object o){
+                    LOG.debug(o);
+                }
+
+                public void println(String s){
+                    LOG.debug(s);
+                }
+
+                public void println(Object o){
+                    LOG.debug(o);
+                }
+
+
+            };
+
+        }
+
+}
