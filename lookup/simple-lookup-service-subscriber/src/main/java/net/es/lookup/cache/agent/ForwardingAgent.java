19b1a0a6ba4b2d1fd336b9981072e0362cd68321
sowmya <sowmya@es.net>
2016-04-19 17:50:13 -0700

    Proof of concept to delete record before inserting in elasticsearch

--- lookup/simple-lookup-service-subscriber/src/main/java/net/es/lookup/cache/agent/ForwardingAgent.java
+++ lookup/simple-lookup-service-subscriber/src/main/java/net/es/lookup/cache/agent/ForwardingAgent.java
@@ -1,8 +1,13 @@
 package net.es.lookup.cache.agent;
 
+import net.es.lookup.common.ReservedKeys;
+import net.es.lookup.common.exception.ParserException;
+import net.es.lookup.protocol.json.JSONParser;
+import net.es.lookup.records.Record;
 import net.sf.json.JSONObject;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -10,6 +15,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Author: sowmya
@@ -43,10 +49,46 @@ public class ForwardingAgent implements Runnable {
 
     @Override
     public void run() {
-      send();
+        deleteOldEntries(); send();
+    }
+
+    private void deleteOldEntries(){
+
+        HttpClient httpclient = new DefaultHttpClient();
+
+        HttpDelete httpDelete = new HttpDelete();
+        httpDelete.setHeader("Accept", "application/json");
+        httpDelete.setHeader("Content-type", "application/json");
+
+        try {
+            Record record = JSONParser.toRecord(data.toString());
+            String uriValue = record.getURI();
+            String uriKey = ReservedKeys.RECORD_URI;
+
+            String delUriString= destination.toString()+"/_search?q="+uriKey+":%22"+uriValue+"%22";
+            System.out.println(delUriString);
+            URI deleteURI = new URI(delUriString);
+
+            httpDelete.setURI(deleteURI);
+
+
+            httpclient.execute(httpDelete);
+
+        } catch (ParserException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public void send(){
+
         HttpClient httpclient = new DefaultHttpClient();
 
         HttpPost httpPost = new HttpPost();
