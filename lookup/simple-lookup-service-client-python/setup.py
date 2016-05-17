be7e3bfc809b06e592560b5246b528eeed4a80ba
sowmyab <sowmyabalasubramanian@lbl.gov>
2016-05-17 15:10:23 -0700

    Updating version

--- lookup/simple-lookup-service-client-python/setup.py
+++ lookup/simple-lookup-service-client-python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="sls-client",
-    version="1.0.12",
+    version="1.0.14",
     description="A python client for the Simple Lookup Service",
     long_description=open("README.rst").read(),
     author=["Andrew Sides", "Sowmya Balasubramanian"],
