e74a0c30ffe957add8581fca305e5aa3fdaf8b46
Kartik Arora <kartikarora99@gmail.com>
2019-07-08 10:09:34 -0700

    update register service to google java format

--- lookup/simple-lookup-service-server/src/main/java/net/es/lookup/api/RegisterService.java
+++ lookup/simple-lookup-service-server/src/main/java/net/es/lookup/api/RegisterService.java
@@ -1,9 +1,6 @@
 package net.es.lookup.api;
 
-import net.es.lookup.common.LeaseManager;
-import net.es.lookup.common.Message;
-import net.es.lookup.common.ReservedKeys;
-import net.es.lookup.common.ReservedValues;
+import net.es.lookup.common.*;
 import net.es.lookup.common.exception.api.BadRequestException;
 import net.es.lookup.common.exception.api.ForbiddenRequestException;
 import net.es.lookup.common.exception.api.InternalErrorException;
@@ -26,147 +23,152 @@ import java.util.UUID;
 
 public class RegisterService {
 
-    private static Logger Log = LogManager.getLogger(RegisterService.class);
+  private static Logger Log = LogManager.getLogger(RegisterService.class);
 
-    public String registerService(String message) {
+  public String registerService(String message) {
 
-        Log.info(" Processing register service.");
-        Log.info(" Received message: " + message);
-        JSONRegisterResponse response;
-        JSONRegisterRequest request = new JSONRegisterRequest(message);
+    Log.info(" Processing register service.");
+    Log.info(" Received message: " + message);
+    JSONRegisterResponse response;
+    JSONRegisterRequest request = new JSONRegisterRequest(message);
 
-        if (request.getStatus() == JSONRegisterRequest.INCORRECT_FORMAT) {
-            Log.info("Register status: FAiled; exiting");
-            Log.error("Incorrect Json Data format");
-            throw new BadRequestException("Error parsing Json elements.");
-        }
-        Log.debug("valid?" + this.isValid(request));
-        if (this.isValid(request) && this.isAuthed(request)) {
-            //Requesting a lease
-            boolean gotLease = LeaseManager.getInstance().requestLease(request);
+    if (request.getStatus() == JSONRegisterRequest.INCORRECT_FORMAT) {
+      Log.info("Register status: FAiled; exiting");
+      Log.error("Incorrect Json Data format");
+      throw new BadRequestException("Error parsing Json elements.");
+    }
+    Log.debug("valid?" + this.isValid(request));
+    if (this.isValid(request) && this.isAuthed(request)) {
+      // Requesting a lease
+      boolean gotLease = LeaseManager.getInstance().requestLease(request);
 
-            if (gotLease) {
+      if (gotLease) {
 
-                String recordType = request.getRecordType();
+        String recordType = request.getRecordType();
 
-                String uri = this.newUri(recordType);
-                request.add(ReservedKeys.RECORD_URI, uri);
+        String uri = this.newUri(recordType);
+        request.add(ReservedKeys.RECORD_URI, uri);
 
-                // Add the state
-                request.add(ReservedKeys.RECORD_STATE, ReservedValues.RECORD_VALUE_STATE_REGISTER);
+        // Add the state
+        request.add(ReservedKeys.RECORD_STATE, ReservedValues.RECORD_VALUE_STATE_REGISTER);
 
-                // Build the matching query requestURl that must fail for the service to be published
-                Message query = new Message();
-                Message operators = new Message();
-//                List<String> list;
-//                java.util.List<String> queryKeyList = new ArrayList();
+        // Build the matching query requestURl that must fail for the service to be published
+        Message query = new Message();
+        Message operators = new Message();
+        //                List<String> list;
+        //                java.util.List<String> queryKeyList = new ArrayList();
 
-                Map<String, Object> keyValues = request.getMap();
-                Iterator it = keyValues.entrySet().iterator();
+        Map<String, Object> keyValues = request.getMap();
+        Iterator it = keyValues.entrySet().iterator();
 
-                while (it.hasNext()) {
+        while (it.hasNext()) {
 
-                    Map.Entry<String, Object> pairs = (Map.Entry) it.next();
+          Map.Entry<String, Object> pairs = (Map.Entry) it.next();
 
-                    if (!isIgnoreKey(pairs.getKey())) {
+          if (!isIgnoreKey(pairs.getKey())) {
 
-                        Log.debug("key-value pair:" + pairs.getKey() + "=" + pairs.getValue());
-                        operators.add(pairs.getKey(), ReservedValues.RECORD_OPERATOR_ALL);
-                        query.add(pairs.getKey(), pairs.getValue());
-                    }
-                }
-
-                try {
-                    ServiceElasticSearch db = new ServiceElasticSearch("127.0.0.1", 9200, 9201, "post");
-                    Message res = db.queryAndPublishService(request);
-                    response = new JSONRegisterResponse(res.getMap());
-                    String responseString;
-                    try {
-                        responseString = JSONMessage.toString(response);
-                    } catch (DataFormatException e) {
-
-                        Log.fatal("Data formatting exception");
-                        Log.info("Register status: FAILED due to Data formatting error; exiting");
-                        throw new InternalErrorException(
-                                "Error in creating response. Data formatting exception at server.");
-                    }
-                    Log.info("Register status: SUCCESS; exiting");
-                    Log.debug("response:" + responseString);
-
-                    // Todo deprecated?
-                    if (PublishService.isServiceOn()) {
-                        Publisher publisher = Publisher.getInstance();
-                        publisher.eventNotification(res);
-                    }
-                    return responseString;
-                } catch (DuplicateEntryException e) {
-                    Log.error("FobiddenRequestException:" + e.getMessage());
-                    Log.info("Register status: FAILED due to Duplicate Entry; exiting");
-                    throw new ForbiddenRequestException(e.getMessage());
-                } catch (Exception e) {
-                    e.printStackTrace(); //Todo
-                }
-            } else {
-
-                // Build response
-                Log.fatal("Failed to secure lease for the registration record");
-                Log.info("Register status: FAILED; exiting");
-                throw new ForbiddenRequestException("Failed to secure lease for the registration record");
-            }
-        } else {
-
-            if (!this.isValid(request)) {
-
-                Log.error("Invalid request");
-                Log.info("Register status: FAILED due to Invalid Request; exiting");
-                throw new BadRequestException("Invalid request. Please check the key-value pairs");
-
-            } else if (!this.isAuthed(request)) {
-
-                Log.error("Not authorized to perform the request");
-                Log.info("Register status: FAILED; exiting");
-                throw new UnauthorizedException("Not authorized to perform the request");
-            }
+            Log.debug("key-value pair:" + pairs.getKey() + "=" + pairs.getValue());
+            operators.add(pairs.getKey(), ReservedValues.RECORD_OPERATOR_ALL);
+            query.add(pairs.getKey(), pairs.getValue());
+          }
         }
 
-        return "\n";
-    }
+        try {
+          ServiceElasticSearch db =
+              new ServiceElasticSearch(
+                  DatabaseConnectionKeys.server,
+                  DatabaseConnectionKeys.DatabasePort1,
+                  DatabaseConnectionKeys.DatabasePort2,
+                  DatabaseConnectionKeys.DatabaseName);
+          Message res = db.queryAndPublishService(request);
+          response = new JSONRegisterResponse(res.getMap());
+          String responseString;
+          try {
+            responseString = JSONMessage.toString(response);
+          } catch (DataFormatException e) {
 
-    private boolean isAuthed(JSONRegisterRequest request) {
+            Log.fatal("Data formatting exception");
+            Log.info("Register status: FAILED due to Data formatting error; exiting");
+            throw new InternalErrorException(
+                "Error in creating response. Data formatting exception at server.");
+          }
+          Log.info("Register status: SUCCESS; exiting");
+          Log.debug("response:" + responseString);
 
-        // The only case where a service registration is denied is when a service with the same name,
-        // same type with
-        // the same client-uuid: this ensures that a service entry with a specified client-uuid cannot
-        // be overwritten.
-        // TODO: needs to be implemented
-        return true;
-    }
-
-    private boolean isValid(JSONRegisterRequest request) {
-        boolean res = request.validate();
-        return (res && request.getRecordType() != null && !request.getRecordType().isEmpty());
-    }
-
-    private String newUri(String recordType) {
-
-        if (recordType != null && !recordType.isEmpty()) {
-            String uri =
-                    LookupService.SERVICE_URI_PREFIX + "/" + recordType + UUID.randomUUID().toString();
-            return uri;
-        } else {
-            Log.error("Error creating URI: Record Type not found");
-            throw new BadRequestException("Cannot create URI. Record Type not found");
+          // Todo deprecated?
+          if (PublishService.isServiceOn()) {
+            Publisher publisher = Publisher.getInstance();
+            publisher.eventNotification(res);
+          }
+          return responseString;
+        } catch (DuplicateEntryException e) {
+          Log.error("FobiddenRequestException:" + e.getMessage());
+          Log.info("Register status: FAILED due to Duplicate Entry; exiting");
+          throw new ForbiddenRequestException(e.getMessage());
+        } catch (Exception e) {
+          e.printStackTrace(); // Todo
         }
+      } else {
+
+        // Build response
+        Log.fatal("Failed to secure lease for the registration record");
+        Log.info("Register status: FAILED; exiting");
+        throw new ForbiddenRequestException("Failed to secure lease for the registration record");
+      }
+    } else {
+
+      if (!this.isValid(request)) {
+
+        Log.error("Invalid request");
+        Log.info("Register status: FAILED due to Invalid Request; exiting");
+        throw new BadRequestException("Invalid request. Please check the key-value pairs");
+
+      } else if (!this.isAuthed(request)) {
+
+        Log.error("Not authorized to perform the request");
+        Log.info("Register status: FAILED; exiting");
+        throw new UnauthorizedException("Not authorized to perform the request");
+      }
     }
 
-    private boolean isIgnoreKey(String key) {
+    return "\n";
+  }
 
-        if (key.equals(ReservedKeys.RECORD_TTL)
-                || key.equals(ReservedKeys.RECORD_EXPIRES)
-                || key.equals(ReservedKeys.RECORD_URI)) {
-            return true;
-        } else {
-            return false;
-        }
+  private boolean isAuthed(JSONRegisterRequest request) {
+
+    // The only case where a service registration is denied is when a service with the same name,
+    // same type with
+    // the same client-uuid: this ensures that a service entry with a specified client-uuid cannot
+    // be overwritten.
+    // TODO: needs to be implemented
+    return true;
+  }
+
+  private boolean isValid(JSONRegisterRequest request) {
+    boolean res = request.validate();
+    return (res && request.getRecordType() != null && !request.getRecordType().isEmpty());
+  }
+
+  private String newUri(String recordType) {
+
+    if (recordType != null && !recordType.isEmpty()) {
+      String uri =
+          LookupService.SERVICE_URI_PREFIX + "/" + recordType + UUID.randomUUID().toString();
+      return uri;
+    } else {
+      Log.error("Error creating URI: Record Type not found");
+      throw new BadRequestException("Cannot create URI. Record Type not found");
     }
+  }
+
+  private boolean isIgnoreKey(String key) {
+
+    if (key.equals(ReservedKeys.RECORD_TTL)
+        || key.equals(ReservedKeys.RECORD_EXPIRES)
+        || key.equals(ReservedKeys.RECORD_URI)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
