3581caa2eb76c1495e75cae2b86b83a8ddc6c43f
sowmyab <sowmyabalasubramanian@lbl.gov>
2022-07-19 17:01:51 -0700

    setting correct permissions for cron script

--- snapshots/install-snapshots.sh
+++ snapshots/install-snapshots.sh
@@ -7,10 +7,11 @@ mkdir -p /var/log/lookup-service/snapshots/
 pip3 install -r snapshots/requirements.txt
 
 #Copy snapshot scripts
-cp snapshots/snapshots.py /usr/lib/lookup-service/snapshots/snapshpts.py
+cp snapshots/snapshots.py /usr/lib/lookup-service/snapshots/snapshots.py
 
 # Add to cron
 cp snapshots/ls-daily-snapshots.sh /etc/cron.daily/ls-daily-snapshots.sh
+chmod 744 /etc/cron.daily/ls-daily-snapshots.sh
 
 
 
